79b8671af526b8dba6f367ffb36b7cdd60585d83
shreyasd-git <82610559+shreyasd-git@users.noreply.github.com>
2021-12-22 22:25:57 +0530

    Added : 'ServletBasic' and 'SubmitFormServlet'
    Added : 'ServletBasic' and 'SubmitFormServlet' (2 Project files)

---
+++ SubmitFormServlet/src/com/servletform/SuccessServlet.java
@@ -0,0 +1,23 @@
+package com.servletform;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SuccessServlet extends HttpServlet {
+	 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		res.setContentType("text/html");
+		PrintWriter p = res.getWriter();
+		p.println("This is Success Page.");
+		p.println("<h2>Successfully Registered!</h2>");
+		
+	}
+
+}
